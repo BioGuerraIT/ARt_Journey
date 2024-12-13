9e4319c485da6bae8bebfe71b56e9df9da004ad9
BioGuerraIT <bioguerrabg@gmail.com>
2024-12-12 20:49:40 -0800

    fixing deploy

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,13 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
   build: {
-    outDir: 'dist'
+    outDir: 'dist',
+    rollupOptions: {
+      input: {
+        main: 'index.html',
+        ar: 'ar.html'
+      }
+    }
   },
   server: {
     port: 5173,
